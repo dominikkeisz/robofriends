e74cba28d7f02c88507cb6d421ce3d77b1c31101
Keisz Dominik <dominikkeisz@Keisz-MacBook-Pro-Chp.local>
2019-12-28 23:43:02 +0100

    Created Card component with data from robots.js and pictures from robohash.org

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+  {
+      id: 1,
+      name: 'Leanne Graham',
+      username: 'Bret',
+      email: 'Sincere@april.biz'
+  },
+  {
+      id: 2,
+      name: 'Ervin Howell',
+      username: 'Antonette',
+      email: 'Shanna@melissa.tv'
+  },
+  {
+      id: 3,
+      name: 'Bettany Powell',
+      username: 'Betty',
+      email: 'Wallrod@messa.tb'
+  },
+  {
+      id: 4,
+      name: 'Solary Lewoh',
+      username: 'Christirand',
+      email: 'Benno@pontfix.ney'
+  }
+];
