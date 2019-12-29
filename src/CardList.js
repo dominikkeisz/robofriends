7d693301114e828f195148920409433659025c9f
Keisz Dominik <dominikkeisz@Keisz-MacBook-Pro-Chp.local>
2019-12-29 12:44:58 +0100

    Refactored index.js with a newly created component called CardList

---
+++ src/CardList.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+    const cardsArray = robots.map((user, i) => {
+        return <Card key={robots[i].id} id={robots[i].id} name={robots[i].name} email={robots[i].email} />
+    });
+
+    return (
+        <Fragment>
+            {cardsArray}
+        </Fragment>
+    );
+}
+
+export default CardList;
